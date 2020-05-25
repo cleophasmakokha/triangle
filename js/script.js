0ca1139f340ae083d600946d2e6c1b9cb56ac810
cleophas <makokhacleophas@gmail.com>
2020-05-25 22:14:36 +0300

    Pushes all the sides into the array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 function triangle() {
   //create an array to store the sides
   var sides = [];
-  
+  //push all the sides into the array
+    sides.push(document.getElementById("num1").value);
+    sides.push(document.getElementById("num2").value);
+    sides.push(document.getElementById("num3").value);
+
 }
