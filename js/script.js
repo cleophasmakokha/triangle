83b57d20b96237d5d98561953b4d6430b7aef5ce
cleophas <makokhacleophas@gmail.com>
2020-05-25 22:46:43 +0300

    Assigns values to the variables

--- js/script.js
+++ js/script.js
@@ -5,5 +5,10 @@ function triangle() {
     sides.push(document.getElementById("num1").value);
     sides.push(document.getElementById("num2").value);
     sides.push(document.getElementById("num3").value);
+    //assign the values of the array to variables a,b,c
+      var [a,b,c] = sides;
+      var display = document.getElementById("display");
+      var explanation = document.getElementById("explanation");
+      if(a.length === 0 || b.length === 0 || c.length === 0 ) {
 
 }
